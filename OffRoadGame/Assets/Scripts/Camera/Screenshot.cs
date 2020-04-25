3fba360e3bffb68b32495e34d8af1ed4358dba71
Roland <10963432+Roland09@users.noreply.github.com>
2020-04-25 09:48:33 +0200

    Initial Commit

---
+++ OffRoadGame/Assets/Scripts/Camera/Screenshot.cs
@@ -0,0 +1,122 @@
+﻿// Screenshot Utility: 
+// 
+// Create a screenshot by pressing a keyboard shortcut. In Unity a "Screenshots" folder is created in paralletl to the "Assets" folder.
+// Screenshots are saved in there.
+//
+// This is a modified version, original license text:
+//
+// Written by Chris Bellini and published as public domain to http://untitledgam.es
+// You may freely use, license or sell any portion of this code for any legal purpose
+// All code is presented as is and without warranty or liability and is used at your own risk
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Screenshot : MonoBehaviour
+{
+    private string screenshotPath;
+
+    [SerializeField]
+    private string inputButton = "Screenshot";
+
+    private bool inputButtonIsMapped;
+
+    [SerializeField]
+    private KeyCode key = KeyCode.F12;
+
+    [SerializeField]
+    [Range(1, 4)]
+    private int superSize = 1;
+
+    public KeyCode Key { get { return this.key; } set { this.key = value; } }
+
+    public int SuperSize { get { return this.superSize; } set { this.superSize = Mathf.Clamp(value, 1, 4); } }
+
+    /// <summary>
+    /// Path for the screenshots: Screenshots in parallel to the Assets folder
+    /// </summary>
+    /// <returns></returns>
+    public string GetPath()
+    {
+
+        string path = Application.dataPath;
+        path = path.Substring(0, path.Length - 7);
+        path = Path.Combine(path, "Screenshots");
+
+        return path;
+    }
+
+    /// <summary>
+    /// Filename is a string formatted as "MyScene - 2018.12.09 - 08.12.28.08.png"
+    /// </summary>
+    /// <returns></returns>
+    private static string GetFilename()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        
+        return string.Format("{0} - {1:yyyy.MM.dd - HH.mm.ss.ff}.png", sceneName, DateTime.Now);
+    }
+
+    public void CaptureScreenshot()
+    {
+        string filepath = Path.Combine(this.screenshotPath, Screenshot.GetFilename());
+        ScreenCapture.CaptureScreenshot(filepath, this.SuperSize);
+
+        Debug.Log(string.Format("[<color=blue>Screenshot</color>] Screenshot captured.\n<color=grey>{0}</color>", filepath));
+    }
+
+
+    /// <summary>
+    /// Set the screenshot path variable and ensure the path exists.
+    /// </summary>
+    private void SetupScreenshotPath()
+    {
+        // ensure the screenshot path exists
+        this.screenshotPath = GetPath();
+
+        if (!Directory.Exists(this.screenshotPath))
+        {
+            Directory.CreateDirectory(this.screenshotPath);
+        }
+
+    }
+
+    private void Awake()
+    {
+        SetupScreenshotPath();
+
+        this.inputButtonIsMapped = this.IsInputButtonMapped();
+    }
+
+    /// <summary>
+    /// Check if "Screenshot" is mapped to another button.
+    /// </summary>
+    /// <returns></returns>
+    private bool IsInputButtonMapped()
+    {
+        if (string.IsNullOrEmpty(this.inputButton))
+        {
+            return false;
+        }
+
+        try
+        {
+            Input.GetButton(this.inputButton);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void Update()
+    {
+        // if screenshot button is pressed, capture a screenshot
+        if (Input.GetKeyDown(this.Key) || (this.inputButtonIsMapped && Input.GetButton(this.inputButton)))
+        {
+            this.CaptureScreenshot();
+        }
+    }
+}
---
+++ OffRoadGame/Assets/Scripts/Enviro/EnviroKeyHandler.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+
+namespace DeveloperTools
+{
+    /// <summary>
+    /// Enviro runtime weather and time modifier.
+    /// Allows you to switch weather and time via keyboard shortcuts.
+    /// Optionally weather and time can be displayed in text (menu UI -> "Text - TextMeshPro") objects in the UI.
+    /// 
+    /// Usage:
+    /// 
+    /// NumPad +/-: shift through predefined list of weather presets
+    /// Alpha [1..0]: directly access a weather preset from a list of weather presets
+    /// PageUp/Down: change time of day by hour
+    /// 
+    /// </summary>
+    public class EnviroKeyHandler : MonoBehaviour
+    {
+        // predefined list in given order
+        /*
+         Weather Prefabs:
+            0: Clear Sky(EnviroWeatherPrefab)
+            1: Cloudy 1 (EnviroWeatherPrefab)
+            2: Cloudy 2 (EnviroWeatherPrefab)
+            3: Cloudy 3 (EnviroWeatherPrefab)
+            4: Cloudy 4 (EnviroWeatherPrefab)
+            5: Foggy 1 (EnviroWeatherPrefab)
+            6: Light Rain(EnviroWeatherPrefab)
+            7: Heavy Rain(EnviroWeatherPrefab)
+            8: Light Snow(EnviroWeatherPrefab)
+            9: Heavy Snow(EnviroWeatherPrefab)
+            10: Storm(EnviroWeatherPrefab)
+
+            (got the info via Log() method below) 
+        */
+        string[] weatherPrefabIdList = new string[] { "Clear Sky", "Cloudy 1", "Cloudy 2", "Cloudy 3", "Cloudy 4", "Foggy", "Light Rain", "Heavy Rain", "Storm", "Light Snow", "Heavy Snow" };
+        int currentWeatherIdIndex = -1;
+
+        public TextMeshProUGUI weatherTextMesh;
+        public TextMeshProUGUI timeTextMesh;
+
+        /// <summary>
+        /// If this is set to true, then the keyboard combinations will only work when ctrl is pressed
+        /// </summary>
+        public bool modifierRequired = false;
+
+        void Start()
+        {
+            UpdateUiText();
+        }
+
+        void Update()
+        {
+            if (EnviroSkyMgr.instance == null || !EnviroSkyMgr.instance.IsStarted())
+                return;
+
+            #region Modifier check
+
+            bool modifierActive = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+
+            #endregion Modifier check
+
+            #region Input Handler
+            if (!modifierRequired || (modifierRequired && modifierActive))
+            {
+
+                #region Time of Day
+                if (Input.GetKeyDown(KeyCode.PageUp))
+                {
+                    EnviroSkyMgr.instance.SetTimeOfDay(EnviroSkyMgr.instance.GetTimeOfDay() + 1f);
+                }
+                else if (Input.GetKeyDown(KeyCode.PageDown))
+                {
+                    EnviroSkyMgr.instance.SetTimeOfDay(EnviroSkyMgr.instance.GetTimeOfDay() - 1f);
+                }
+                #endregion Time of Day
+
+                #region Weather
+                if (Input.anyKeyDown)
+                {
+                    currentWeatherIdIndex = weatherPrefabIdList.ToList().IndexOf(EnviroSkyMgr.instance.GetCurrentWeatherPreset().name);
+
+                    int originalWeatherIndexId = currentWeatherIdIndex;
+
+                    if (Input.GetKeyDown(KeyCode.KeypadPlus))
+                    {
+                        currentWeatherIdIndex++;
+                        if (currentWeatherIdIndex == weatherPrefabIdList.Length)
+                        {
+                            currentWeatherIdIndex = 0;
+                        }
+                    }
+                    else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+                    {
+                        currentWeatherIdIndex--;
+                        if (currentWeatherIdIndex < 0)
+                        {
+                            currentWeatherIdIndex = weatherPrefabIdList.Length - 1;
+                        }
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha1))
+                    {
+                        currentWeatherIdIndex = 0;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha2))
+                    {
+                        currentWeatherIdIndex = 1;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha3))
+                    {
+                        currentWeatherIdIndex = 2;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha4))
+                    {
+                        currentWeatherIdIndex = 3;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha5))
+                    {
+                        currentWeatherIdIndex = 4;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha6))
+                    {
+                        currentWeatherIdIndex = 5;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha7))
+                    {
+                        currentWeatherIdIndex = 6;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha8))
+                    {
+                        currentWeatherIdIndex = 7;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha9))
+                    {
+                        currentWeatherIdIndex = 8;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha0))
+                    {
+                        currentWeatherIdIndex = 9;
+                    }
+
+                    // weather changed
+                    if (currentWeatherIdIndex != originalWeatherIndexId)
+                    {
+                        EnviroSkyMgr.instance.ChangeWeather(weatherPrefabIdList[currentWeatherIdIndex]);
+                    }
+                }
+                #endregion Weather
+            }
+            #endregion Input Handler
+
+            // update ui weather and time information
+            UpdateUiText();
+
+        }
+
+        /// <summary>
+        /// Update the text objects in the UI with information about time and weather
+        /// </summary>
+        void UpdateUiText()
+        {
+            if (weatherTextMesh != null)
+            {
+                // show weather in gui; getting the preset and using that one in order to avoid nullpointer exceptions
+                EnviroWeatherPreset preset = EnviroSkyMgr.instance.Weather.currentActiveWeatherPreset;
+                if (preset != null)
+                {
+                    weatherTextMesh.text = preset.name;
+                }
+            }
+
+            // time
+            if (timeTextMesh != null)
+            {
+                timeTextMesh.text = EnviroSkyMgr.instance.GetTimeString();
+            }
+        }
+
+    }
+}
